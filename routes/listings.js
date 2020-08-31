8df556d6cd4116e26df8d547e74cb82e4794060a
Kristina Calumpong <kcalumpong@gmail.com>
2020-08-31 10:01:12 -0700

    changed data structure of images

--- routes/listings.js
+++ routes/listings.js
@@ -7,14 +7,10 @@ router.get('/:listingId', function(req, res, next) {
     'heading': 'This is a Listing Header',
     'description': 'This is a listing description. This is possibly a long string with manny paragraphs.',
     'seller_id': 1234567890,
-    'images': {
-      itemId1: [
-        'https://colorlib.com/preview/theme/shoppers/images/cloth_1.jpg'
-      ],
-      itemId2: [
+    'images': [
+        'https://colorlib.com/preview/theme/shoppers/images/cloth_1.jpg',
         'https://colorlib.com/preview/theme/shoppers/images/cloth_2.jpg'
-      ]
-    },
+    ],
     'price': 199.99
   };
   res.json(listingData);
